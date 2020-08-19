2f245cd42c35022b8291514f9f585b2fde36e09d
mogira <7379940+mogira@users.noreply.github.com>
2020-08-19 14:26:29 +0900

    Add git configs

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-for file in $(find $script_dir -type f -name ".*"); do
-    ln -snfv $file ~/
+
+excludes=('.gitignore')
+
+for file in $(find ${script_dir} -type f -name ".*"); do
+    if ! $(echo ${excludes[@]} | grep -q $(basename ${file})); then
+        ln -snfv ${file} ~/
+    fi
 done
 
